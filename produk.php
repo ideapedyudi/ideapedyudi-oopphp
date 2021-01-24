689652b4ae1a996c43fe66a18409a2049c4ace8e
muhammad wahyudi xplacebo <ideapedyudi@gmail.com>
2021-01-23 21:20:10 -0800

    property dan method

--- produk.php
+++ produk.php
@@ -3,7 +3,38 @@
 
 class Produk
 {
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = "0";
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperti = "hahahahah";
+// var_dump($produk2->judul);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Mashasi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 25000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
